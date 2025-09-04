d2d9530166bbddd2872e96afcb64c98ab01fe166
oscarchantre <oscardavids_10@hotmail.com>
2025-09-04 13:42:59 -0500

    nuevo ejercicio practico

--- POM/objetos.py
+++ POM/objetos.py
@@ -1,34 +1,41 @@
 import re
 from playwright.sync_api import Page, expect
 
+class HomePage:
+    def __init__(self, page: Page):
+        self.page = page
 
-def test_example(page: Page) -> None:
-    page.goto("https://demoqa.com/")
-    page.locator("div:nth-child(2) > div > .avatar > svg").click()
-    page.get_by_text("Practice Form").click()
-    page.get_by_role("textbox", name="First Name").click()
-    page.get_by_role("textbox", name="First Name").fill("oscar")
-    page.get_by_role("textbox", name="Last Name").click()
-    page.get_by_role("textbox", name="Last Name").fill("chantre")
-    page.get_by_role("textbox", name="name@example.com").click()
-    page.get_by_role("textbox", name="name@example.com").fill("odchantre")
-    page.get_by_role("textbox", name="name@example.com").press("Alt+ControlOrMeta+@")
-    page.get_by_role("textbox", name="name@example.com").press("Alt+6")
-    page.get_by_role("textbox", name="name@example.com").press("Alt+4")
-    page.get_by_role("textbox", name="name@example.com").fill("odchantre@hotmail.com")
-    page.get_by_text("Male", exact=True).click()
-    page.get_by_role("textbox", name="Mobile Number").click()
-    page.get_by_role("textbox", name="Mobile Number").fill("3167485525")
-    page.locator("#dateOfBirthInput").click()
-    page.get_by_role("option", name="Choose Monday, August 4th,").click()
-    page.locator(".subjects-auto-complete__value-container").click()
-    page.locator("#subjectsInput").fill("prueba")
-    page.get_by_text("Sports").click()
-    page.get_by_role("textbox", name="Current Address").click()
-    page.get_by_role("textbox", name="Current Address").fill("prueba")
-    page.locator("div").filter(has_text=re.compile(r"^Select State$")).nth(3).click()
-    page.get_by_text("Haryana", exact=True).click()
-    page.get_by_text("Select City").click()
-    page.get_by_text("Panipat", exact=True).click()
-    page.get_by_role("button", name="Submit").click()
-    page.get_by_role("button", name="Close").click()
+    def open(self):
+        self.page.goto("https://demoqa.com/")
+
+    def menu(self):
+        self.locator("span").filter(has_text="Forms").locator("div").first.click()
+        self.get_by_text("Practice Form").click()
+
+    def diligenciar_formulario(self, First_Name, LastName, email, mobile, current_addres):
+        self.get_by_role("textbox", name="First Name").click()
+        self.get_by_role("textbox", name="First Name").fill(First_Name)
+        self.get_by_role("textbox", name="First Name").press("Tab")
+        self.get_by_role("textbox", name="Last Name").fill(LastName)
+        self.get_by_role("textbox", name="name@example.com").click()
+        self.get_by_role("textbox", name="name@example.com").fill(email)
+        self.get_by_text("Male", exact=True).click()
+        self.get_by_role("textbox", name="Mobile Number").click()
+        self.get_by_role("textbox", name="Mobile Number").fill(mobile)
+        self.locator("#dateOfBirthInput").click()
+        self.get_by_role("combobox").nth(1).select_option("1991")
+        self.locator("div").filter(has_text=re.compile(r"^JanuaryFebruaryMarchAprilMayJuneJulyAugustSeptemberOctoberNovemberDecember$")).get_by_role("combobox").select_option("2")
+        self.get_by_role("option", name="Choose Saturday, March 2nd,").click()
+        self.locator(".subjects-auto-complete__value-container").click()
+        self.locator("#subjectsInput").fill("Esto es una prueba")
+        self.get_by_text("Sports").click()
+        self.get_by_role("textbox", name="Current Address").click()
+        self.get_by_role("textbox", name="Current Address").fill(current_addres)
+        self.get_by_text("Select State").click()
+        self.get_by_text("NCR", exact=True).click()
+        self.get_by_text("Select City").click()
+        self.get_by_text("Delhi", exact=True).click()
+
+    def finalizar(self):
+        self.get_by_role("button", name="Submit").click()
+        self.get_by_role("button", name="Close").click()
